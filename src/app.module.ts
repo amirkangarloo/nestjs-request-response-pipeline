b342606afaa1e0fb105731857226ae0d7eed9817
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-12-04 11:09:24 +0330

    Implement AuthGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './controller';
+import { AuthGuard } from './guard/auth.guard';
 import { AuthMiddleware } from './middleware';
 import { AppService, RequestService } from './service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, RequestService],
+  providers: [
+    AppService,
+    RequestService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 4000;
   await app.listen(port);
+  Logger.log(`App is listen on port: ${port} ...`)
 }
 bootstrap();
